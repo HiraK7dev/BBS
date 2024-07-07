ad2c085d5b7034c1943e05d8136b0c1d4880fa09
hirak <hirak.bhatta2004@gmail.com>
2024-07-07 22:01:28 +0530

    designed user screen

--- app/(tabs)/home/[user].jsx
+++ app/(tabs)/home/[user].jsx
@@ -1,25 +1,103 @@
-import { View, Text } from 'react-native'
-import React, { useContext, useEffect } from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router'
-import { Icon } from 'react-native-paper';
-import { datacontext } from '../../../context/DataContext';
-import CustomHeader from '../../../components/CustomHeader';
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { DataTable, Icon } from "react-native-paper";
+import { datacontext } from "../../../context/DataContext";
+import CustomHeader from "../../../components/CustomHeader";
 
 const User = () => {
-    const userId = useLocalSearchParams();
+  const userId = useLocalSearchParams();
 
-    const { data } = useContext(datacontext);
+  const { data } = useContext(datacontext);
+  const { name, location, yearPaid } = data.documents[userId.user];
 
   return (
     <View>
-        <Stack.Screen
-            options={{
-                headerTitle: props => <CustomHeader title={data.documents[userId.user]?.name} {...props}/>
-              }}
+      <Stack.Screen
+        options={{
+          headerTitle: (props) => (
+            <CustomHeader
+              title={name}
+              {...props}
             />
-      <Text>{userId.user}</Text>
+          ),
+        }}
+      />
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}>Location</Text>
+        <Text style={styles.locationText}>
+          {location}
+        </Text>
+        <View style={styles.divider} />
+        <Text style={styles.headerText}>Family Details</Text>
+        <View style={styles.tableContainer}>
+          <DataTable>
+            {/* Header */}
+            <DataTable.Header>
+              <DataTable.Title>Gender</DataTable.Title>
+              <DataTable.Title numeric>No. of Members</DataTable.Title>
+            </DataTable.Header>
+            {/* Body */}
+            <DataTable.Row>
+              <DataTable.Cell>Male</DataTable.Cell>
+              <DataTable.Cell numeric>3</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell>Female</DataTable.Cell>
+              <DataTable.Cell numeric>1</DataTable.Cell>
+            </DataTable.Row>
+          </DataTable>
+        </View>
+        <View style={styles.divider} />
+        <Text style={styles.headerText}>Yearly Contribution</Text>
+        <View style={styles.tableContainer}>
+          <DataTable>
+            {/* Header */}
+            <DataTable.Header>
+              <DataTable.Title>Year</DataTable.Title>
+              <DataTable.Title numeric>Amount(₹)</DataTable.Title>
+            </DataTable.Header>
+            {/* Body */}
+            {JSON.parse(yearPaid).map((item) => {
+              return (
+                <DataTable.Row key={item.id}>
+                  <DataTable.Cell>{item.y}</DataTable.Cell>
+                  <DataTable.Cell numeric>{item.p}</DataTable.Cell>
+                </DataTable.Row>
+              );
+            })}
+          </DataTable>
+        </View>
+        <View style={styles.safeArea} />
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default User
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  divider: {
+    height: 20,
+  },
+  safeArea: {
+    height: 40,
+  },
+  locationText: {
+    borderRadius: 15,
+    backgroundColor: "#C2BBF0",
+    padding: 10,
+  },
+  tableContainer: {
+    borderRadius: 15,
+    backgroundColor: "#C2BBF0",
+  },
+  headerText: {
+    fontWeight: "900",
+    fontSize: 25,
+    marginBottom: 15,
+  },
+});
+
+export default User;
